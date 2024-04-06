4753a7f729c5087d6491f2217fa62d0c8a41792b
Jay Cortes <jaycortes@Jays-Air.fritz.box>
2024-04-06 18:24:16 +0200

    first commit

---
+++ giraffe.rb
@@ -0,0 +1,30 @@
+name = "Jay"
+age = 33
+status = "Married"
+occupation = "Software Developer"
+
+puts name
+puts age 
+puts status
+
+Class Vehicle {
+
+   Number no_of_wheels
+   Number horsepower
+   Characters type_of_tank
+   Number Capacity
+   Function speeding {
+
+   }
+   
+   Function driving {
+   }
+   
+   Function halting {
+   }
+}
+
+def greeting(jay)
+    puts "Hello, #{name}!"
+    return
+  end
