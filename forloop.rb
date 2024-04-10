c73fc360035588d46b37feaa8ea5a64de1f7575e
Jay Cortes <jaycortes@Jays-Air.fritz.box>
2024-04-10 12:34:08 +0200

    Including more information such as loops and reader of text

---
+++ forloop.rb
@@ -0,0 +1,31 @@
+
+#Using do and element inside of bars
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+friends.each do |friend|
+  puts friend
+end
+
+
+#Classic method.
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+for element in friends
+  puts element
+end
+
+
+#Index of array in For Loop
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+for index in 0..5
+  puts index
+end
+
+
+
+#Obtaining index using do and bars
+
+6.times do |whateverelement|
+  puts whateverelement
+end
--- guessinggame.rb
+++ guessinggame.rb
@@ -1,12 +1,24 @@
 secret_word = "jirafa"
 guess = ""
 message1 = "Cual es el animal secreto?"
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
 
 puts message1
-while guess != secret_word
 
-  puts "Enter your guess: "
-  guess = gets.chomp()
+while guess != secret_word and !out_of_guesses
+  if guess_count <  guess_limit
+    puts "Enter your guess: "
+    guess = gets.chomp()
+    guess_count += 1
+  else
+    out_of_guesses = true
+  end
 end
 
-puts "You won!"
+if out_of_guesses
+  puts "You Lose"
+else
+  puts "You Won"
+end
